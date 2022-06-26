55dcf2a318be405103682977bd68b246331c000d
Naner Nunes <naner@naner.com.br>
2022-06-26 18:32:47 -0300

    Covering floats and not supported types

--- envs_test.go
+++ envs_test.go
@@ -13,6 +13,7 @@ type FromEnvs struct {
 	Port int
 	Live bool
 	Dead bool
+	Math float64
 	Text string `default:"example"`
 	TheZ string `underscore:"false"`
 	Just string `env:"JUST"`
@@ -33,6 +34,7 @@ func (suite *EnvTestSuite) SetupSuite() {
 	os.Setenv("MYAPP_LIVE", "true")
 	os.Setenv("MYAPP_DEAD", "0")
 	os.Setenv("MYAPP_THEZ", "Z")
+	os.Setenv("MYAPP_MATH", "3.14")
 	os.Setenv("JUST", "alone")
 
 	Process("MYAPP", &suite.StructWithVariables)
@@ -41,6 +43,7 @@ func (suite *EnvTestSuite) SetupSuite() {
 func (suite *EnvTestSuite) TestAssertingAllVariablesWereLoadedWithTheRightCasting() {
 	assert.Equal(suite.T(), "localhost", suite.StructWithVariables.Host)
 	assert.Equal(suite.T(), 9999, suite.StructWithVariables.Port)
+	assert.Equal(suite.T(), 3.14, suite.StructWithVariables.Math)
 	assert.Equal(suite.T(), true, suite.StructWithVariables.Live)
 	assert.Equal(suite.T(), false, suite.StructWithVariables.Dead)
 }
@@ -55,4 +58,19 @@ func (suite *EnvTestSuite) TestShouldNotIncludeUnderscoreDuringTheLookupForAnEnv
 
 func (suite *EnvTestSuite) TestShouldIgnoreThePrefixWhenDefinedWithTheKeywordEnv() {
 	assert.Equal(suite.T(), "alone", suite.StructWithVariables.Just)
+}
+
+func (suite *EnvTestSuite) TestShouldRaisePanicWhenANotCoveredTypedIsUsedAsEnv() {
+	os.Setenv("MYAPP_ELSE", "{}")
+
+	type ShouldPanic struct {
+		Else struct{}
+	}
+
+	var sp ShouldPanic
+
+	assert.Panics(suite.T(), func() {
+		Process("MYAPP", &sp)
+	})
+
 }
